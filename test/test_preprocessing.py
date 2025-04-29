f5b5e9fd502ecb3e2d27e5d7df9ebf14cfecccc8
Nels Schimek <nlsschim@uw.edu>
2025-04-28 17:00:27 -0700

    testing codecov

--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -27,23 +27,24 @@ class TestPreprocessing():
 
     def test_image_list_split(self):
         ##Just copying the main code from the .ipynb, not a test yet hehes
-        folder_location = 'example_dataset'
-        arr = os.listdir(folder_location)
-        folder_list = np.asarray(arr)
-        folder_list = [ x for x in folder_list if "DS" not in x ]
-        files_to_split_list = []
-        expected_file_list = []
+        # folder_location = 'example_dataset'
+        # arr = os.listdir(folder_location)
+        # folder_list = np.asarray(arr)
+        # folder_list = [ x for x in folder_list if "DS" not in x ]
+        # files_to_split_list = []
+        # expected_file_list = []
 
-        for folders in folder_location:
-            image_array = os.listdir(str(folder_location + '/' + folders))
-            subfolder_list = np.asarray(image_array)
-            subfolder_list = [ x for x in subfolder_list if "DS" not in x]
-            for subfolders in subfolder_list:
-                image_array = os.listdir(str(folder_location + '/' + folders + '/' + subfolders))
-                files_list = np.asarray(image_array)
-                files_list = [x for x in files_list if "DS" not in x]
-                for files in files_list:
-                    name = str(folder_location + '/' + folders + '/' + subfolders + '/' + files)
-                    files_to_split_list.append(name)
+        # for folders in folder_location:
+        #     image_array = os.listdir(str(folder_location + '/' + folders))
+        #     subfolder_list = np.asarray(image_array)
+        #     subfolder_list = [ x for x in subfolder_list if "DS" not in x]
+        #     for subfolders in subfolder_list:
+        #         image_array = os.listdir(str(folder_location + '/' + folders + '/' + subfolders))
+        #         files_list = np.asarray(image_array)
+        #         files_list = [x for x in files_list if "DS" not in x]
+        #         for files in files_list:
+        #             name = str(folder_location + '/' + folders + '/' + subfolders + '/' + files)
+        #             files_to_split_list.append(name)
 
-        self.assertListEqual(files_to_split_list, expected_file_list)
+        # self.assertListEqual(files_to_split_list, expected_file_list)
+        pass
