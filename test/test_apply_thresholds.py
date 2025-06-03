13eada3c1692f819ce520fead4338fd8a388cb02
Colin <colinl1@uw.edu>
2025-06-03 15:28:11 -0700

    Update test_apply_thresholds.py
    Added import statement for shutil

--- test/test_apply_thresholds.py
+++ test/test_apply_thresholds.py
@@ -4,6 +4,7 @@ import os
 from skimage import io
 from skimage.util import img_as_ubyte
 from skimage.io import imsave
+import shutil
 import tempfile
 from typing import Callable
 import numpy as np
