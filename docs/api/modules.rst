062f0f4ddfea2d0ba96a897974a1062fcb57f391
Nels Schimek <nlsschim@uw.edu>
2025-09-09 20:17:54 -0700

    read the docs

---
+++ docs/api/modules.rst
@@ -0,0 +1,21 @@
+API Reference
+=============
+
+This section contains the complete API documentation for TURMorIC.
+
+Core Modules
+------------
+
+.. toctree::
+   :maxdepth: 2
+   
+   image_processing
+   thresholding
+   cell_analysis
+   vampire_models
+   utilities
+
+.. automodule:: turmoric
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,110 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../src/'))
 
+# -- Project information -----------------------------------------------------
 project = 'TURMorIC'
-copyright = '2025, Nels Schimek, Colin Landis'
-author = 'Nels Schimek, Colin Landis'
+copyright = '2025, Nels Schimek, Krista Phommatha, Colin Landis, Muna Yase, Heather Wood, Sergi Mayta'
+author = 'Nels Schimek, Krista Phommatha, Colin Landis, Muna Yase, Heather Wood, Sergi Mayta'
 release = '0.0.1'
+version = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.linkcode',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#2980B9',
+    'collapse_navigation': False,  # Keep navigation expanded
+    'sticky_navigation': True,
+    'navigation_depth': 5,  # Increased depth for better navigation
+    'includehidden': True,
+    'titles_only': False
+}
+
 html_static_path = ['_static']
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_rtd_theme', 'sphinx.ext.viewcode']
+# Add custom CSS
+html_css_files = [
+    'custom.css',
+]
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__',
+    'show-inheritance': True,
+}
+
+# Autosummary settings
+autosummary_generate = True
+autosummary_generate_overwrite = True
+
+# Better function signature display
+autodoc_typehints = 'description'
+autodoc_typehints_description_target = 'documented'
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'skimage': ('https://scikit-image.org/docs/stable/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+}
+
+# Link to source code on GitHub
+def linkcode_resolve(domain, info):
+    """Determine the URL corresponding to Python object."""
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+    
+    filename = info['module'].replace('.', '/')
+    return f"https://github.com/Nance-Lab/TURMorIC/blob/main/src/{filename}.py"
+
+# Better section ordering
+toc_object_entries_show_parents = 'hide'
